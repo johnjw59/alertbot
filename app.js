94bcb2d15252fddf0122ec4326a19acedc6a520d
John Wiebe <johnjw59@gmail.com>
2023-02-05 01:21:32 -0800

    Get a messag sent out to discord webhook

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+// Module includes
+require('dotenv').config();
+const { WebhookClient } = require('discord.js');
+
+const webhookClient = new WebhookClient({ id: process.env.DISCORD_WEBHOOK_ID, token: process.env.DISCORD_WEBHOOK_TOKEN });
+
+webhookClient.send({
+  content: 'Webhook test',
+});
