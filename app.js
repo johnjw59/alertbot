3f788bfecf4ef9e98b980a057ea1f52e0b75cf62
John Wiebe <johnjw59@gmail.com>
2023-02-05 03:09:07 -0800

    Add event class to handle emitting/listening to events between modules

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-// Module includes
+'use strict';
+
 require('dotenv').config();
 const { WebhookClient } = require('discord.js');
+const alertEvents = require('./lib/AlertEvents');
 
 const webhookClient = new WebhookClient({ id: process.env.DISCORD_WEBHOOK_ID, token: process.env.DISCORD_WEBHOOK_TOKEN });
 
-webhookClient.send({
-  content: 'Webhook test',
+// Listen for alerts and send the included messages to Discord.
+alertEvents.onAlert((message) => {
+  webhookClient.send({
+    content: message,
+  });
 });
