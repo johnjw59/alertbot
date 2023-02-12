ac81634b380a02935dd4592f3fc86fe774b31763
John Wiebe <johnjw59@gmail.com>
2023-02-11 19:05:37 -0800

    Use arrow thats supported on iOS in gaswizard message

--- alerts/GasWizard/index.js
+++ alerts/GasWizard/index.js
@@ -23,7 +23,7 @@ function GasWizard() {
           if ($el.text().indexOf('Vancouver') !== -1) {
             // Price for regular is the first price element.
             const $price_elem = $($('.gwgp-price', $el.parent())[0]);
-            const change = $('.price-direction', $price_elem).text();
+            let change = $('.price-direction', $price_elem).text();
 
             // If there's a change, send a message about it.
             if (change != 'n/c') {
@@ -34,6 +34,7 @@ function GasWizard() {
                   'dddd Do of MMMM YYYY'
                 ).format('X')),
               };
+              change = parseInt(change.substring(2));
 
               // Keep track of our data.
               const old_data = store.get('gaswizard.latest', { date: -1 });
@@ -43,7 +44,9 @@ function GasWizard() {
               // But make sure we're looking at a new date. If we've seen this
               // date before, only continue if the price has changed.
               if ((data.date > old_data.date) || ((data.date == old_data.date) && (data.price != old_data.price))) {
-                alertEvents.emitAlert(`Gas prediction for ${moment(data.date, 'X').format('LL')}: (${change}) ${data.price}/L`);
+                alertEvents.emitAlert(
+                  `Gas prediction for ${moment(data.date, 'X').format('LL')}: (${($change > 0) ? '↑' : '↓'} ${change}) ${data.price}/L`
+                );
               }
             }
           }
