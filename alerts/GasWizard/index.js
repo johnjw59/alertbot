6d4f996d887ebf381446a9a1e93111462ca6bf89
John Wiebe <johnjw59@gmail.com>
2024-04-19 02:26:27 -0700

    Update scrapping for changes in website

--- alerts/GasWizard/index.js
+++ alerts/GasWizard/index.js
@@ -12,45 +12,44 @@ function GasWizard() {
   const schedule_rule = '0 30 10 * * *';
 
   const job = schedule.scheduleJob(schedule_rule, () => {
-    axios.get('https://gaswizard.ca/gas-price-predictions/')
+    axios.get('https://gaswizard.ca/gas-prices/vancouver/')
       .then(({ data }) => {
         const $ = cheerio.load(data);
 
-        // Find the row for Vancouver.
-        $('td.gwgp-cityname').each((i, el) => {
-          const $el = $(el);
+        const $row = $('ul.single-city-prices li').first();
+        const $price_elem = $row.find('.fueltype .fuelprice').first();
+        const $change_elem = $price_elem.find('.price-direction');
 
-          if ($el.text().indexOf('Vancouver') !== -1) {
-            // Price for regular is the first price element.
-            const $price_elem = $($('.gwgp-price', $el.parent())[0]);
-            let change = $('.price-direction', $price_elem).text();
+        if (!$change_elem.hasClass('pd-nc')) {
+          const date = parseInt(moment(
+            $row.find('.datetext').text().trim(),
+            'MMM D, YYYY'
+          ).format('X'));
 
-            // If there's a change, send a message about it.
-            if (change != 'n/c') {
-              const data = {
-                price: $price_elem.contents()[0].nodeValue.trim(),
-                date: parseInt(moment(
-                  $('.price-date').text().substring(21),
-                  'dddd Do of MMMM YYYY'
-                ).format('X')),
-              };
-              change = parseInt(change.substring(1, change.length - 1));
+          let price = $price_elem.contents()[0].nodeValue.trim();
+          price = price.substring(0, price.length - 1);
 
-              // Keep track of our data.
-              const old_data = store.get('gaswizard.latest', { date: -1 });
-              store.set('gaswizard.latest', data);
+          let change = $change_elem.text();
+          change = change.substring(1, change.length - 1);
 
-              // Send the alert!
-              // But make sure we're looking at a new date. If we've seen this
-              // date before, only continue if the price has changed.
-              if ((data.date > old_data.date) || ((data.date == old_data.date) && (data.price != old_data.price))) {
-                alertEvents.emitAlert(
-                  `Gas prediction for ${moment(data.date, 'X').format('LL')}: (${(change > 0) ? '↑' : '↓'} ${change}) ${data.price}/L`
-                );
-              }
-            }
+          const data = {
+            price: price,
+            date: date,
+          };
+
+          // Keep track of our data.
+          const old_data = store.get('gaswizard.latest', { date: -1 });
+          store.set('gaswizard.latest', data);
+
+          // Send the alert!
+          // But make sure we're looking at a new date. If we've seen this
+          // date before, only continue if the price has changed.
+          if ((data.date > old_data.date) || ((data.date == old_data.date) && (data.price != old_data.price))) {
+            alertEvents.emitAlert(
+              `Gas prediction for ${moment(data.date, 'X').format('LL')}: (${(change > 0) ? '↑' : '↓'} ${change}) $${data.price}/L`
+            );
           }
-        });
+        }
       });
   });
 }
